07f0e944330781f76531a1bc80b0477f47bb7902
ImperatorMarsa <tima.sitnikov@mail.ru>
2022-10-30 15:33:55 +0300

    Настроил проект для работы с 'GH pages'

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sierpinski-triangle-generetor-with-vue',
   plugins: [vue()],
   resolve: {
     alias: {
